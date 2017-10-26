28a8604e000a94de8465dc7ff854684080ce81e6
inkysigma <inkysigma@outlook.com>
2017-10-26 12:41:31 -0700

    Ported build stack
    replaced writeDebugStreamLine with printf

---
+++ include/ops/build_stack.c
@@ -0,0 +1,49 @@
+void buildStack(int current_level) {
+	// start with claw at top and get cone and return to top to dispense
+	printf("	raising lift {\n");
+	raiseLiftTo(current_level, true);
+	printf("	}\n");
+
+	printf("	lowering claw fully {\n");
+	lowerClawPartial(true);
+	printf("	}\n");
+
+
+	printf("	lowering the lift to ground state {\n");
+	lowerLiftTo(0, false);
+	printf("	}\n");
+
+	printf("	lowering claw fully {\n");
+	lowerClawFully();
+	printf("	}\n");
+
+	delay(750);
+
+	printf("	closing the claw fully {\n");
+	closeClawFully();
+	printf("	}\n");
+
+	// extend the claw a bit further so that we don't get caugh
+	printf("	raising the claw partially {\n");
+	raiseClawPartial(true);
+	writeDebugStreamLine("	}");
+
+	// swtich the claw back on top
+	printf("	raising the lift back up {");
+	raiseLiftTo(current_level, true);
+	printf("	}\n");
+
+	printf("	raising claw fully {\n");
+	raiseClawFully(true);
+	printf("	}\n");
+
+	printf("	lowering lift {\n");
+	lowerLiftTo(current_level, true);
+	printf("	}\n");
+
+	delay(400);
+
+	printf("	releasing cone {\n");
+	releaseCone(false);
+	printf("	}\n");
+}
