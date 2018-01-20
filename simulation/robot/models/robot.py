3f4d32d2f42778e6ba8bfbef95fff48849a78d29
AK9dev <ak9dev@gmail.com>
2018-01-19 21:19:46 -0800

    started simulation

---
+++ simulation/robot/models/robot.py
@@ -0,0 +1,19 @@
+import pymunk
+import pygame.image
+import os
+from .. import ROOT_DIR
+
+
+class Robot:
+    def __init__(self, space, size=(20, 20)):
+        self.mass = 10
+        self.body = pymunk.Body(self.mass, pymunk.moment_for_box(self.mass, size))
+        self.box = pymunk.Poly.create_box(self.body)
+        space.add(self.body)
+        self.image = pygame.image.load(os.path.join(ROOT_DIR, "resources/robot.png"))
+
+    def __convert(self, position):
+        return (position)
+
+    def draw(self, screen):
+        pos = self.box.body.position
